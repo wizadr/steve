3e8bb14a5072c7c372af3be0276c9a950ff68b6a
Steve Rodrigue <stev.rodr@gmail.com>
2022-01-24 10:28:34 -0500

    Added the genesis builder interfaces

--- domain/genesis/sdk.go
+++ domain/genesis/sdk.go
@@ -7,6 +7,14 @@ import (
 	"github.com/steve-care-software/digital-diamonds/domain/hash"
 )
 
+// Builder represents a genesis builder
+type Builder interface {
+	Create() Builder
+	WithUnits(units Units) Builder
+	CreatedOn(createdOn time.Time) Builder
+	Now() (Genesis, error)
+}
+
 // Genesis represents a genesis
 type Genesis interface {
 	Hash() hash.Hash
@@ -14,10 +22,25 @@ type Genesis interface {
 	CreatedOn() time.Time
 }
 
+// UnitsBuilder represents a units builder
+type UnitsBuilder interface {
+	Create() UnitsBuilder
+	WithList(list []Unit) UnitsBuilder
+	Now() (Units, error)
+}
+
 // Units represents genesis units
 type Units interface {
 	Hash() hash.Hash
 	All() []Unit
+}
+
+// UnitBuilder represents a unit builder
+type UnitBuilder interface {
+	Create() UnitBuilder
+	WithContent(content UnitContent) UnitBuilder
+	WithSignature(sig signature.Signature) UnitBuilder
+	Now() (Unit, error)
 }
 
 // Unit represents a genesis unit
@@ -27,10 +50,21 @@ type Unit interface {
 	Signature() signature.Signature
 }
 
+// UnitContentBuilder represents the unit content builder
+type UnitContentBuilder interface {
+	Create() UnitContentBuilder
+	WithAmount(amount uint) UnitContentBuilder
+	WithNonce(nonce string) UnitContentBuilder
+	ActivatedOn(activatedOn time.Time) UnitContentBuilder
+	CreatedOn(createdOn time.Time) UnitContentBuilder
+	Now() (UnitContent, error)
+}
+
 // UnitContent represents a unit content
 type UnitContent interface {
 	Hash() hash.Hash
 	Amount() uint
 	Nonce() string
+	ActivatedOn() time.Time
 	CreatedOn() time.Time
 }
