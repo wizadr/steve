07907a3e34fb0b04e3e72cd658826c90e31d38e7
Steve Rodrigue <stev.rodr@gmail.com>
2022-01-24 10:33:57 -0500

    Added the receipt builder

--- domain/receipts/sdk.go
+++ domain/receipts/sdk.go
@@ -1,10 +1,18 @@
 package receipts
 
 import (
-	"github.com/steve-care-software/digital-diamonds/domain/transactions/secrets"
 	"github.com/steve-care-software/digital-diamonds/domain/cryptography/keys/signature"
 	"github.com/steve-care-software/digital-diamonds/domain/hash"
+	"github.com/steve-care-software/digital-diamonds/domain/transactions/secrets"
 )
+
+// Builder represents a receipt builder
+type Builder interface {
+	Create() Builder
+	WithSecret(secret secrets.Secret) Builder
+	WithSignature(signature signature.Signature) Builder
+	Now() (Receipt, error)
+}
 
 // Receipt represents a transaction receipt
 type Receipt interface {
