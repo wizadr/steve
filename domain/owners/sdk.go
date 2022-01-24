fb47da8a9235aeecc7895beeb5a238a1241475b9
Steve Rodrigue <stev.rodr@gmail.com>
2022-01-24 10:32:43 -0500

    Added the owners builder

--- domain/owners/sdk.go
+++ domain/owners/sdk.go
@@ -2,10 +2,24 @@ package owners
 
 import "github.com/steve-care-software/digital-diamonds/domain/hash"
 
+// Builder represents the owners builder
+type Builder interface {
+	Create() Builder
+	WithList(list []Owner) Builder
+	Now() (Owners, error)
+}
+
 // Owners represents owners
 type Owners interface {
 	Hash() hash.Hash
 	List() []Owner
+}
+
+// OwnerBuilder represents the owner builder
+type OwnerBuilder interface {
+	Create() OwnerBuilder
+	WithHashes(hashes []hash.Hash) OwnerBuilder
+	Now() (Owner, error)
 }
 
 // Owner represents an owner
