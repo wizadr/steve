68cddeee7319e2c1b709ea76f62805c6335793fc
Steve Rodrigue <stev.rodr@gmail.com>
2022-01-24 10:30:34 -0500

    Added the identity builder

--- domain/identities/sdk.go
+++ domain/identities/sdk.go
@@ -5,6 +5,15 @@ import (
 	"github.com/steve-care-software/digital-diamonds/domain/transactions/secrets"
 )
 
+// Builder represents the identity builder
+type Builder interface {
+	Create() Builder
+	WithName(name string) Builder
+	WithOutgoing(outgoing privates.Privates) Builder
+	WithIncoming(incoming secrets.Secrets) Builder
+	Now() (Identity, error)
+}
+
 // Identity represents the identity
 type Identity interface {
 	Name() string
